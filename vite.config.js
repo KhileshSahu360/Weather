d5c5efd42a6d4f96f8d169b71c27f203e8a7a0b3
KhileshSahu360 <khileshanjole@gmail.com>
2024-03-02 11:59:15 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+  // import { defineConfig } from 'vite';
+  // import { VitePWA } from 'vite-plugin-pwa';
+  // import { ViteAliases } from 'vite-aliases';
+  // import { VitePluginPages } from 'vite-plugin-pages';
+  // import { VitePluginVuedoc } from 'vite-plugin-vuedoc';
+  // import ViteComponents from 'vite-plugin-components';
+  // import { ViteIconsResolver } from 'vite-plugin-icons';
+  // import path from 'path';
+  // import VitePluginGhPages from 'vite-plugin-gh-pages'; // Add this line
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+  // export default defineConfig({
+  //   plugins: [
+  //     VitePWA(),
+  //     ViteAliases(),
+  //     VitePluginPages(),
+  //     VitePluginVuedoc(),
+  //     ViteComponents(),
+  //     ViteIconsResolver(),
+  //     VitePluginGhPages({ // Add this plugin configuration
+  //       build: {
+  //         outDir: 'dist', // Specify your build output directory
+  //       },
+  //     }),
+  //   ],
+  //   resolve: {
+  //     alias: {
+  //       '@': path.resolve(__dirname, 'src'),
+  //     },
+  //   },
+  // });
